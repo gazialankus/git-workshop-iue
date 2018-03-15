d15b5cc702d09cadfd649d76cd2dde755fad4d81
Gazihan Alankus <gazialankus@gmail.com>
2018-03-15 18:26:34 +0300

    Added all

---
+++ src/com/company/Employee.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Employee {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Employee e = new Employee();
+
+        int sum = e.add(5, 6);
+
+        System.out.println(sum);
+        System.out.println("new line");
+    }
+}
