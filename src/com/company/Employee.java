0605ac8990018cfd8b420eda817b65e5c88c67cc
Gazihan Alankus <gazialankus@gmail.com>
2018-03-15 18:40:48 +0300

    Another line

--- src/com/company/Employee.java
+++ src/com/company/Employee.java
@@ -5,6 +5,7 @@ public class Employee {
     public int add(int a, int b) {
         System.out.println("line1");
         System.out.println("line2");
+        System.out.println("line2");
         return a + b;
     }
 
