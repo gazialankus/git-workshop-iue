5c6d725aabce024321c1c035c5b72be8130bc641
Gazihan Alankus <gazialankus@gmail.com>
2019-11-12 11:51:34 +0300

    Change 3 with multiple lines.

--- src/com/company/Employee.java
+++ src/com/company/Employee.java
@@ -5,6 +5,22 @@ public class Employee {
     public int add(int a, int b) {
         System.out.println("change 1");
         System.out.println("line1");
+        System.out.println("change 3");
+        System.out.println("change 3.");
+        System.out.println("change 3");
+        System.out.println("change 3.");
+        System.out.println("change 3");
+        System.out.println("change 3.");
+        System.out.println("change 3");
+        System.out.println("change 3.");
+        System.out.println("change 3");
+        System.out.println("change 3.");
+        System.out.println("change 3");
+        System.out.println("change 3.");
+        System.out.println("change 3");
+        System.out.println("change 3.");
+        System.out.println("change 3");
+        System.out.println("change 3.");
         System.out.println("line2");
         System.out.println("line2");
         return a + b;
